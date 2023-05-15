d35a41177886062539364803d19a58325b9bd8c1
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-05-15 17:27:58 +0700

    upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dientich
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float chieurong;
+            float chieudai;
+            Console.Write("Nhap chieu rong: ");
+            chieurong = float.Parse(Console.ReadLine());
+            Console.Write("nhap chieu dai: ");
+            chieudai= float.Parse(Console.ReadLine());
+            float dientich = chieurong * chieudai;
+            Console.WriteLine("dientich la " + dientich);
+        }
+    }
+}
